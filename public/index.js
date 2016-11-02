c9f2e194b1608e38428d64651cf11d9fec19e85d
Inoue Daiki <daikinissan@gmail.com>
2016-11-03 03:14:17 +0900

    remove point

--- public/index.js
+++ public/index.js
@@ -54,6 +54,6 @@ var chart = Highcharts.chart('container', {
 socket.on('data', function(raw_data) {
   chart.series[0].addPoint([Date.now(), raw_data]);
   if (chart.series[0].data.length > 128 * 5) {
-    chart.series[0].removePoint(0);
+    chart.series[0].removePoint(chart.series[0].data[0].x);
   }
 });
