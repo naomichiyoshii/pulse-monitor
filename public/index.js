f4d7c94384a2101513b8f3b23b3993b92ac12689
Inoue Daiki <daikinissan@gmail.com>
2016-11-03 03:03:28 +0900

    Set data directly

--- public/index.js
+++ public/index.js
@@ -1,64 +1,59 @@
 var socket = io();
-
-data = [
-  [Date.now(), Math.random() * 1024]
-];
-
-socket.on('data', function(raw_data) {
-  console.log('data: ' + raw_data);
-  data.push([Date.now(), raw_data]);
-  if (data.length > 128 * 5) {
-    data.splice(0, 1);
-  }
-  $('#container').highcharts({
-    chart: {
-      type: 'spline'
-    },
+var chart = Highcharts.chart('container', {
+  chart: {
+    type: 'spline'
+  },
+  title: {
+    text: 'RRI計測テスト用グラフ',
+    x: -20 //center
+  },
+  xAxis: {
     title: {
-      text: 'RRI計測テスト用グラフ',
-      x: -20 //center
+      text: 'time(ms)'
     },
-    xAxis: {
-      title: {
-        text: 'time(ms)'
-      },
-      labels: {
+    labels: {
+      enabled: false
+    },
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  yAxis: {
+    title: {
+      text: 'RRI(ms)'
+    },
+    max: 1024,
+    min: 0,
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  tooltip: {
+    valueSuffix: 'ms'
+  },
+  plotOptions: {
+    spline: {
+      marker: {
         enabled: false
-      },
-      plotLines: [{
-        value: 0,
-        width: 1,
-        color: '#808080'
-      }]
-    },
-    yAxis: {
-      title: {
-        text: 'RRI(ms)'
-      },
-      max: 1024,
-      min: 0,
-      plotLines: [{
-        value: 0,
-        width: 1,
-        color: '#808080'
-      }]
-    },
-    tooltip: {
-      valueSuffix: 'ms'
-    },
-    plotOptions: {
-      spline: {
-        marker: {
-          enabled: false
-        }
-      },
-      series: {
-        animation: false
       }
     },
-    series: [{
-      name: 'RRI',
-      data: data
-    }]
-  });
+    series: {
+      animation: false
+    }
+  },
+  series: [{
+    name: 'RRI',
+    data: []
+  }]
+});
+
+socket.on('data', function(raw_data) {
+  chart.series[0].setData([Date.now(), raw_data]);
+  if(chart.series[0].length>128*5){
+    chart.series[0].removePoint(0);
+  }
 });
