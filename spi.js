c748bce878e8776b5142bac88c4bfdee24ef1fa7
Yoshii Naomichi <naomichi.yoshii.7f@gmail.com>
2016-11-03 00:29:14 +0900

    Initital commit

---
+++ spi.js
@@ -0,0 +1,22 @@
+var SPI = require('pi-spi');
+var spi = SPI.initialize("/dev/spidev0.0");
+var MCP3002 = Buffer([0x68, 0]);
+
+var pulseSPI = {};
+pulseSPI.start = function(freq){
+  if (!freq) {
+    var freq = 128;
+  }
+  setInterval(function() {
+    spi.transfer(MCP3002, MCP3002.length, function (e,d) {
+      if (e) {
+        console.error(e);
+      } else {
+        var v = ((d[0]<<8) + d[1]) & 0x03FF
+        console.log(v, "Got \""+v.toString()+"\" back.");
+      }
+  	  });
+  }, 1000 / freq);
+};
+
+module.exports = pulseSPI;
