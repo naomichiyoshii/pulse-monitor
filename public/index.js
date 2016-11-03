c419e4c5a5f17db08f7b2b470ad9f22f07aae08d
Inoue Daiki <daikinissan@gmail.com>
2016-11-03 11:44:55 +0900

    FFT

--- public/index.js
+++ public/index.js
@@ -50,8 +50,60 @@ var chart = Highcharts.chart('container', {
     data: [512,512,512,512,512,512,512,512,512,512,512,512,512,512,512,512]
   }]
 });
-
+var fft_chart = Highcharts.chart('fft', {
+  chart: {
+    type: 'areaspline'
+  },
+  title: {
+    text: 'FFTによるスペクトル解析結果',
+    x: -20 //center
+  },
+  xAxis: {
+    title: {
+      text: 'frequency(kHz)'
+    },
+    labels: {
+      enabled: false
+    },
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  yAxis: {
+    title: {
+      text: 'power'
+    },
+    max: 1024,
+    min: 0,
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  plotOptions: {
+    spline: {
+      marker: {
+        enabled: false
+      }
+    },
+    series: {
+      animation: false
+    }
+  },
+  series: [{
+    name: 'Power',
+    data: []
+  }]
+});
 socket.on('data', function(raw_data) {
   chart.series[0].removePoint(0);
   chart.series[0].addPoint(raw_data);
+});
+socket.on('fft', function(fft_data) {
+  var fft_series = [];
+
+  chart.series[0].setData(fft_series);
 });
--- spi.js
+++ spi.js
@@ -1,13 +1,16 @@
 var SPI = require('pi-spi');
 var spi = SPI.initialize("/dev/spidev0.0");
 var MCP3002 = Buffer([0x68, 0]);
+var fft = require('fft-js').fft;
+var fftUtil = require('fft-js').util;
+var data = [];
 
 var pulseSPI = {};
-pulseSPI.start = function(server, freq){
+pulseSPI.start = function(server, freq) {
   var io = require('socket.io')(server);
-  io.on('connection', function(socket){
+  io.on('connection', function(socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
       console.log('user disconnected');
     });
   });
@@ -15,15 +18,29 @@ pulseSPI.start = function(server, freq){
     var freq = 128;
   }
   setInterval(function() {
-    spi.transfer(MCP3002, MCP3002.length, function (e,d) {
+    spi.transfer(MCP3002, MCP3002.length, function(e, d) {
       if (e) {
         console.error(e);
       } else {
-        var v = ((d[0]<<8) + d[1]) & 0x03FF
-        console.log(v, "Got \""+v.toString()+"\" back.");
+        var v = ((d[0] << 8) + d[1]) & 0x03FF
+        console.log(v, "Got \"" + v.toString() + "\" back.");
         io.emit("data", v);
+        data.push(v);
+        if (data.length > 16) {
+          data.splice(0, data.length - 16);
+          var phasors = fft(signal);
+          var frequencies = fftUtil.fftFreq(phasors, freq); // Sample rate and coef is just used for length, and frequency step
+          var magnitudes = fftUtil.fftMag(phasors);
+          var both = frequencies.map(function(f, ix) {
+            return {
+              frequency: f,
+              magnitude: magnitudes[ix]
+            };
+          });
+          io.emit("fft", both);
+        }
       }
-  	  });
+    });
   }, 1000 / freq);
 };
 
