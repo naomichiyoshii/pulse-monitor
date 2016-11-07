0754f7abf1f6a5829ec8aef6cba83a4049b1beb9
Inoue Daiki <daikinissan@gmail.com>
2016-11-07 17:17:15 +0900

    use

--- spi.js
+++ spi.js
@@ -5,41 +5,51 @@ var fft = require('fft-js').fft;
 var fftUtil = require('fft-js').util;
 var data = [];
 var oldData = 512;
+var gpio = require("gpio");
 
 var pulseSPI = {};
 pulseSPI.start = function(server, freq) {
-  var io = require('socket.io')(server);
-  io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function() {
-      console.log('user disconnected');
-    });
+
+  var gpio4 = gpio.export(4, {
+    direction: 'out',
+    ready: function() {
+      gpio4.set(function() {
+
+        var io = require('socket.io')(server);
+        io.on('connection', function(socket) {
+          console.log('a user connected');
+          socket.on('disconnect', function() {
+            console.log('user disconnected');
+          });
+        });
+        if (!freq) {
+          var freq = 128;
+        }
+        setInterval(function() {
+          spi.transfer(MCP3002, MCP3002.length, function(e, d) {
+            if (e) {
+              console.error(e);
+            } else {
+              var v = ((d[0] << 8) + d[1]) & 0x03FF;
+              data.push(v);
+              if (data.length > 512) {
+                data.splice(0, 1);
+              }
+              if (data.length > 1) {
+                var args = data.slice(data.length - Math.pow(2, Math.floor(Math.LOG2E * Math.log(data.length))));
+                var phasors = fft(args);
+                var frequencies = fftUtil.fftFreq(phasors, 1); // Sample rate and coef is just used for length, and frequency step
+                var magnitudes = fftUtil.fftMag(phasors);
+                io.emit("data", args);
+                io.emit("fft", frequencies, magnitudes);
+                console.log(Date.now());
+              }
+            }
+          });
+        }, 100);
+      });
+    }
   });
-  if (!freq) {
-    var freq = 128;
-  }
-  setInterval(function() {
-    spi.transfer(MCP3002, MCP3002.length, function(e, d) {
-      if (e) {
-        console.error(e);
-      } else {
-        var v = ((d[0] << 8) + d[1]) & 0x03FF;
-        data.push(v);
-        if (data.length > 512) {
-          data.splice(0,1);
-        }
-        if (data.length > 1) {
-          var args = data.slice(data.length - Math.pow(2, Math.floor(Math.LOG2E * Math.log(data.length))));
-          var phasors = fft(args);
-          var frequencies = fftUtil.fftFreq(phasors, 1); // Sample rate and coef is just used for length, and frequency step
-          var magnitudes = fftUtil.fftMag(phasors);
-          io.emit("data", args);
-          io.emit("fft", frequencies, magnitudes);
-          console.log(Date.now());
-        }
-      }
-    });
-  }, 100);
 };
 
 module.exports = pulseSPI;
