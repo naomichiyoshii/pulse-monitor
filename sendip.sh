9a5f7b06cec8c4046c4965798c99b34f4771b8dd
Inoue Daiki <daikinissan@gmail.com>
2016-11-17 17:19:11 +0900

    send ip with bash

--- sendip.sh
+++ sendip.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 res=`/sbin/ifconfig -a | grep inet[^6] | sed 's/.*inet[^6][^0-9]*\([0-9.]*\)[^0-9]*.*/\1/' | grep -v '^127\.'`
 d1="curl -X POST -H 'Content-type:application/json' --data '{\"text\":\""
 d2="\"}' https://hooks.slack.com/services/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"
