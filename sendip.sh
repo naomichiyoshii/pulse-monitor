2c59a4f11d61e906cef99be13ede280cc1a91b02
Yoshii Naomichi <naomichi.yoshii.7f@gmail.com>
2016-11-17 17:39:19 +0900

    Enable sendip.sh

--- sendip.sh
+++ sendip.sh
@@ -3,5 +3,5 @@
 res=`/sbin/ifconfig -a | grep inet[^6] | sed 's/.*inet[^6][^0-9]*\([0-9.]*\)[^0-9]*.*/\1/' | grep -v '^127\.'`
 
 curl -X POST \
---data-urlencode 'payload={"text":"Raspberry pi booted on"}' \
+--data-urlencode 'payload={"text":"Raspberry pi booted on '$res'"}' \
 https://hooks.slack.com/services/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
