1846495ca103a5fe1842b9b73cdc3f4e6b632708
Inoue Daiki <daikinissan@gmail.com>
2016-11-17 17:06:48 +0900

    Delete one space in sendip.sh

--- sendip.sh
+++ sendip.sh
@@ -1,5 +1,5 @@
 res=`/sbin/ifconfig -a | grep inet[^6] | sed 's/.*inet[^6][^0-9]*\([0-9.]*\)[^0-9]*.*/\1/'`
-d1="curl -X POST -H 'Content-type: application/json' --data '{\"text\":\""
+d1="curl -X POST -H 'Content-type:application/json' --data '{\"text\":\""
 d2="\"}' https://hooks.slack.com/services/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"
 cmd=$d1$res$d2
 $cmd
