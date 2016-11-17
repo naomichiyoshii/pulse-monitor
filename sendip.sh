d7b913f1677411b4340250133a0989123a976423
Inoue Daiki <daikinissan@gmail.com>
2016-11-17 17:24:55 +0900

    Simplify sendip

--- sendip.sh
+++ sendip.sh
@@ -4,5 +4,4 @@ res=`/sbin/ifconfig -a | grep inet[^6] | sed 's/.*inet[^6][^0-9]*\([0-9.]*\)[^0-
 d1="curl -X POST -H 'Content-type:application/json' --data '{\"text\":\""
 d2="\"}' https://hooks.slack.com/services/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"
 echo $d1$res$d2
-res=`$d1$res$d2`
-echo $res
+$d1$res$d2
