872ea6348bde7af45b9a452d919adfd46dbf841a
YoshiiNaomichi <naomichi.yoshii.7f@stu.hosei.ac.jp>
2016-12-02 12:21:48 +0900

    GoogleSpreadsheet addRow after data push

--- spi.js
+++ spi.js
@@ -32,124 +32,137 @@ var credentials = require('./My Project-fe2e8436da48.json');
 var sheet;
 
 pulseSPI.start = function(server, freq) {
-  gpio4 = gpio.export(4, {
-    direction: 'out',
-    ready: function() {
-      gpio4.set(function() {
-        var io = require('socket.io')(server);
-        io.on('connection', function(socket) {
-          console.log('a user connected');
-          socket.on('disconnect', function() {
-            console.log('user disconnected');
-          });
-        });
-        if (!freq) {
-          var freq = 128;
-        }
-        setInterval(function() {
-          spi.transfer(MCP3002, MCP3002.length, function(e, d) {
-            if (e) {
-              console.error(e);
-            } else {
-              //High-pass filter
-              if(before_v == 0){
-                 var v = ((d[0] << 8) + d[1]) & 0x03FF;
-                 before_v = v;
-              }else{
-                 lowpath_v =　lowpath * before_v + (1 - lowpath) * ((d[0] << 8) + d[1]) & 0x03FF;
-                 before_v = lowpath_v;
-                 var v = (((d[0] << 8) + d[1]) & 0x03FF) - lowpath_v;
-                  }
-              // Low-pass filter
-              // if(before_v == 0){
-              //    var v = ((d[0] << 8) + d[1]) & 0x03FF;
-              //    before_v = v;
-              // }else{
-              //   var v = (((d[0] << 8) + d[1]) & 0x03FF);
-              //   lowpath_v =　(1 - lowpath) * before_v + lowpath * v;
-              //   before_v = lowpath_v;
-              // }
-              console.log(v);
-              data2.push(v);
-              if (data2.length > 256) {
-                data2.splice(0, 1);
-              }
-              if(v > BL && !boo){
-                if(v < lastv){
-                  var nowTime = new Date();
-                  if(lastTime == 0){
-                    lastTime = nowTime;
-                    masterTime = nowTime;
-                    boo = true;
-                  }else{
-                      if(lasty != 0 && lastRRI != 0 && skiptimes == 0 && (nowTime - lastTime < 350  || nowTime - lastTime > 1350) && (nowTime - masterTime > lastRRI + 100 || nowTime - masterTime < lastRRI - 100)) {
-                        console.log("線形補間: " + lasty);
-                        data.push(lasty);
-                        console.log("RRIデータ数: " + data.length);
-                        skiptimes += 1;
-                        return false;
-                      }
-                      skiptimes = 0;
-                      lastRRI = RRI;
-                      RRI = nowTime - lastTime;
-                      boo = true;
-                      if(nowTime - masterTime > x + 1000){
-                         x += (Math.floor(((nowTime - masterTime) - x) / 1000)) * 1000;
-                      }
-                      if(nowTime - masterTime > x){
-                        y = lastRRI + (x + (masterTime - lastTime)) * (RRI - lastRRI) / (nowTime - lastTime);
-                        if(lasty == 0 && (y > 350  && y < 1350)){
-                          lasty = y;
-                        }
-                        if(lasty != 0 && (y > 350  && y < 1350) && ( y > (lasty/2) && y < (lasty*1.5) )){
-                          lasty = y;
-                          console.log("線形補間: " + y);
-                          data.push(y);
-                          console.log("RRIデータ数: " + data.length);
-                        }else{
-                          console.log("線形補間: " + lasty);
-                          data.push(lasty);
-                          console.log("RRIデータ数: " + data.length);
-                        }
-                        x += 1000;
-                      }
-                      lastTime = nowTime;
-                  }
-                }
-                lastv = v;
-              }else if (v < BL && boo) {
-                boo = false;
-              }
-              if (data.length > 512) {
-                data.splice(0, 1);
-              }
-              if (data.length > 1) {
-                my_sheet.useServiceAccountAuth(credentials, function(err){
-                    my_sheet.getInfo(function(err, sheetsdata){
-                    sheet = sheetsdata; //あとから使えるように外部スコープに保存
-                      for(var i in sheet.worksheets) {
-                          if(sheet.worksheets[i].title === 'sheet3') {
-                              dataset["col1"] = data[data.length-1];
-                              sheet.worksheets[i].addRow(dataset);
-                          }
-                      }
-                    });
+  my_sheet.useServiceAccountAuth(credentials, function(err){
+      my_sheet.getInfo(function(err, sheetsdata){
+        sheet = sheetsdata; //あとから使えるように外部スコープに保存
+        gpio4 = gpio.export(4, {
+          direction: 'out',
+          ready: function() {
+            gpio4.set(function() {
+              var io = require('socket.io')(server);
+              io.on('connection', function(socket) {
+                console.log('a user connected');
+                socket.on('disconnect', function() {
+                  console.log('user disconnected');
                 });
-                var args = data.slice(data.length - Math.pow(2, Math.floor(Math.LOG2E * Math.log(data.length))));
-                var phasors = fft(args);
-                var frequencies = fftUtil.fftFreq(phasors, 1); // Sample rate and coef is just used for length, and frequency step
-                var magnitudes = fftUtil.fftMag(phasors);
-                io.emit("data", args);
-                //io.emit("fft", frequencies, magnitudes);
-                io.emit("data2", data2);
+              });
+              if (!freq) {
+                var freq = 128;
               }
-            }
-          });
-        }, 50);
-      });
-    }
+              setInterval(function() {
+                spi.transfer(MCP3002, MCP3002.length, function(e, d) {
+                  if (e) {
+                    console.error(e);
+                  } else {
+                    //High-pass filter
+                    if(before_v == 0){
+                       var v = ((d[0] << 8) + d[1]) & 0x03FF;
+                       before_v = v;
+                    }else{
+                       lowpath_v =　lowpath * before_v + (1 - lowpath) * ((d[0] << 8) + d[1]) & 0x03FF;
+                       before_v = lowpath_v;
+                       var v = (((d[0] << 8) + d[1]) & 0x03FF) - lowpath_v;
+                        }
+                    // Low-pass filter
+                    // if(before_v == 0){
+                    //    var v = ((d[0] << 8) + d[1]) & 0x03FF;
+                    //    before_v = v;
+                    // }else{
+                    //   var v = (((d[0] << 8) + d[1]) & 0x03FF);
+                    //   lowpath_v =　(1 - lowpath) * before_v + lowpath * v;
+                    //   before_v = lowpath_v;
+                    // }
+                    console.log(v);
+                    data2.push(v);
+                    if (data2.length > 256) {
+                      data2.splice(0, 1);
+                    }
+                    if(v > BL && !boo){
+                      if(v < lastv){
+                        var nowTime = new Date();
+                        if(lastTime == 0){
+                          lastTime = nowTime;
+                          masterTime = nowTime;
+                          boo = true;
+                        }else{
+                            if(lasty != 0 && lastRRI != 0 && skiptimes == 0 && (nowTime - lastTime < 350  || nowTime - lastTime > 1350) && (nowTime - masterTime > lastRRI + 100 || nowTime - masterTime < lastRRI - 100)) {
+                              console.log("線形補間: " + lasty);
+                              data.push(lasty);
+                              for(var i in sheet.worksheets) {
+                                  if(sheet.worksheets[i].title === 'sheet3') {
+                                      dataset["col1"] = lasty;
+                                      sheet.worksheets[i].addRow(dataset);
+                                  }
+                              }
+                              console.log("RRIデータ数: " + data.length);
+                              skiptimes += 1;
+                              return false;
+                            }
+                            skiptimes = 0;
+                            lastRRI = RRI;
+                            RRI = nowTime - lastTime;
+                            boo = true;
+                            if(nowTime - masterTime > x + 1000){
+                               x += (Math.floor(((nowTime - masterTime) - x) / 1000)) * 1000;
+                            }
+                            if(nowTime - masterTime > x){
+                              y = lastRRI + (x + (masterTime - lastTime)) * (RRI - lastRRI) / (nowTime - lastTime);
+                              if(lasty == 0 && (y > 350  && y < 1350)){
+                                lasty = y;
+                              }
+                              if(lasty != 0 && (y > 350  && y < 1350) && ( y > (lasty/2) && y < (lasty*1.5) )){
+                                lasty = y;
+                                console.log("線形補間: " + y);
+                                data.push(y);
+                                for(var i in sheet.worksheets) {
+                                    if(sheet.worksheets[i].title === 'sheet3') {
+                                        dataset["col1"] = y;
+                                        sheet.worksheets[i].addRow(dataset);
+                                    }
+                                }
+                                console.log("RRIデータ数: " + data.length);
+                              }else{
+                                console.log("線形補間: " + lasty);
+                                data.push(lasty);
+                                for(var i in sheet.worksheets) {
+                                    if(sheet.worksheets[i].title === 'sheet3') {
+                                        dataset["col1"] = lasty;
+                                        sheet.worksheets[i].addRow(dataset);
+                                    }
+                                }
+                                console.log("RRIデータ数: " + data.length);
+                              }
+                              x += 1000;
+                            }
+                            lastTime = nowTime;
+                        }
+                      }
+                      lastv = v;
+                    }else if (v < BL && boo) {
+                      boo = false;
+                    }
+                    if (data.length > 512) {
+                      data.splice(0, 1);
+                    }
+                    if (data.length > 1) {
+                      var args = data.slice(data.length - Math.pow(2, Math.floor(Math.LOG2E * Math.log(data.length))));
+                      var phasors = fft(args);
+                      var frequencies = fftUtil.fftFreq(phasors, 1); // Sample rate and coef is just used for length, and frequency step
+                      var magnitudes = fftUtil.fftMag(phasors);
+                      io.emit("data", args);
+                      //io.emit("fft", frequencies, magnitudes);
+                      io.emit("data2", data2);
+                    }
+                  }
+                });
+              }, 50);
+            });
+          }
+        });
+    });
   });
 };
+
 
 process.on('SIGINT', function() {
   console.log("Stop server");
