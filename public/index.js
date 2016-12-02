039feef6932ac949c8ed52e62694fa5604fd3b7e
YoshiiNaomichi <naomichi.yoshii.7f@stu.hosei.ac.jp>
2016-12-02 11:44:49 +0900

    GoogleSpreadsheet test

--- public/index.js
+++ public/index.js
@@ -1,29 +1,9 @@
 var socket = io();
 
 function startRRI(){
-  var GoogleSpreadsheet = require('google-spreadsheet');
-  var my_sheet = new GoogleSpreadsheet("1_blqESLe2bVW3yUqcXVRejwtizhntQBNv__wv3ZY0ww");
-  var credentials = require('./google-generated-creds.json');
-  var sheet;
   var ret = confirm("RRIの計測を開始します。よろしいですか？");
   if(ret == true){
-    my_sheet.useServiceAccountAuth(credentials, function(err){
-        my_sheet.getInfo(function(err, data){
-          console.log(data);
-          sheet = data; //あとから使えるように外部スコープに保存
-          for(var i in sheet.worksheets) {
-              if(sheet.worksheets[i].title === 'シート1') {
-                  sheet.worksheets[i].getRows( function( err, rows ) {
-                      for(var i in rows) {
-                          console.log(rows[i]);
-                      }
-                  });
-              }
-          }
-        });
 
-
-    });
   }
 }
 
@@ -177,4 +157,5 @@ socket.on('data', function(raw_data) {
 socket.on('fft', function(frequencies, magnitudes) {
   fft_chart.xAxis[0].setCategories(frequencies);
   fft_chart.series[0].setData(magnitudes);
-});
+}
+);
--- test.js
+++ test.js
@@ -1,23 +1,33 @@
 var GoogleSpreadsheet = require('google-spreadsheet');
 
+var dataset = {};
+
 var my_sheet = new GoogleSpreadsheet("1_blqESLe2bVW3yUqcXVRejwtizhntQBNv__wv3ZY0ww");
-var credentials = require('./google-generated-creds.json');
+var credentials = require('./My Project-fe2e8436da48.json');
 
 var sheet;
+setInterval(function() {
 my_sheet.useServiceAccountAuth(credentials, function(err){
     my_sheet.getInfo(function(err, data){
-      console.log(data);
+      //console.log(data);
       sheet = data; //あとから使えるように外部スコープに保存
       for(var i in sheet.worksheets) {
-          if(sheet.worksheets[i].title === 'シート1') {
-              sheet.worksheets[i].getRows( function( err, rows ) {
-                  for(var i in rows) {
-                      console.log(rows[i]);
-                  }
-              });
+          if(sheet.worksheets[i].title === 'sheet2') {
+              // sheet.worksheets[i].getRows( function( err, rows ) {
+              //     for(var i in rows) {
+              //         console.log(rows[i]);
+              //     }
+              // });
+              var nowTime = new Date();
+              dataset["col1"] = nowTime;
+              dataset["col2"] = "愛生";
+              dataset["tiba"] = "千葉";
+              sheet.worksheets[i].addRow(dataset);
           }
       }
     });
 
 
 });
+
+}, 1000);
