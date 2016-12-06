7882ade78d638959041b2787a0c4de87282f839f
Inoue Daiki <daikinissan@gmail.com>
2016-12-06 13:56:35 +0900

    binding in connection

--- spi.js
+++ spi.js
@@ -44,10 +44,10 @@ pulseSPI.start = function(server, freq) {
                 socket.on('disconnect', function() {
                   console.log('user disconnected');
                 });
-              });
-              io.on('startUpload', function(socket) {
-                console.log('Start Upload');
-                managingSheets();
+                socket.on('startUpload', function(socket) {
+                  console.log('Start Upload');
+                  managingSheets();
+                });
               });
               if (!freq) {
                 var freq = 128;
