fcbdabd98f83d4e7366a2a4b400902760a56f428
YoshiiNaomichi <naomichi.yoshii.7f@stu.hosei.ac.jp>
2016-12-12 17:30:55 +0900

    jfjgsks

--- spi.js
+++ spi.js
@@ -171,10 +171,11 @@ function dataCalc() {
       if (data.length > 1) {
         var args = data.slice(data.length - Math.pow(2, Math.floor(Math.LOG2E * Math.log(data.length))));
         var phasors = fft(args);
+        console.log(phasors);
         var frequencies = fftUtil.fftFreq(phasors, 1); // Sample rate and coef is just used for length, and frequency step
         var magnitudes = fftUtil.fftMag(phasors);
         io.emit("data", args);
-        //io.emit("fft", frequencies, magnitudes);
+        io.emit("fft", frequencies, magnitudes);
         io.emit("rawData", rawData);
       } else {
         io.emit("rawData", rawData);
