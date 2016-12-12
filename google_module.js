0fba4cf3a29eefcedf1e19978cd626f384dc33a9
YoshiiNaomichi <naomichi.yoshii.7f@stu.hosei.ac.jp>
2016-12-12 18:11:29 +0900

    parse int

--- google_module.js
+++ google_module.js
@@ -214,8 +214,7 @@ google_module.setAnalysisData = function(callback) {
     if (response.values.length > 0) {
       responseRRI = [];
       for (var i = 0; i < response.values.length; i++) {
-        responseRRI.push(response.values[i]);
-        console.log("Added:" + response.values[i]);
+        responseRRI.push(parseInt(response.values[i]));
       }
       console.log("Get RRI");
     }
