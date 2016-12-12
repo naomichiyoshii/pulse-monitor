0989d7ab465f8541a03a1d547f9a48e4284a101d
YoshiiNaomichi <naomichi.yoshii.7f@stu.hosei.ac.jp>
2016-12-12 18:08:08 +0900

    lk

--- testofspi.js
+++ testofspi.js
@@ -187,7 +187,8 @@ function dataCalc() {
 
 function startAnalysis(){
     console.log(fft);
-    var fftargs = responseRRI.slice(responseRRI.length - Math.pow(2, Math.floor(Math.LOG2E * Math.log(responseRRI.length))));
+    var fftargs = [557,702,704,688,781,628,737,651,664,733,580,752,706,777,777,790,650,652,846,751,796,707,707,766,803,803,1078,1140,1140,708,744,616,721,689,637,797,739,741,846,720,808,822,742,918,780,886,826,809,823,753,665,840,776,652,652,947,802,682,698,744,672,713,735,613,749,556,556,725,688,655,660,763,867,763,874,909,832,886,709,791,781,803,801,852,835,831,888,810,802,781,709,846,732,803,698,665,794,685,677,665,737,706,772,734,626,761,709,709,720,812,766,732,718,703,702,751,752,752,710,693,780,760,745,772,853,755,877,813];
+    // var fftargs = responseRRI.slice(responseRRI.length - Math.pow(2, Math.floor(Math.LOG2E * Math.log(responseRRI.length))));
     console.log('fftargs: ' + fftargs);
     var phasors = fft(fftargs);
     console.log(phasors);
