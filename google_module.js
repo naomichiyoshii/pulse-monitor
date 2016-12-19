ac10f6a11bd8ff064295eedd457c482e32d03486
YoshiiNaomichi <naomichi.yoshii.7f@stu.hosei.ac.jp>
2016-12-19 15:18:12 +0900

    lpjtgui

--- google_module.js
+++ google_module.js
@@ -205,7 +205,7 @@ google_module.appendFFTData = function(data) {
     auth: auth,
     spreadsheetId: SPREADSHEET_ID,
     valueInputOption: "USER_ENTERED",
-    range: "sheet11!C2",
+    range: "sheet12!C2",
     resource: {
       values: data
     }
@@ -222,7 +222,7 @@ google_module.setAnalysisData = function(callback) {
   sheets.spreadsheets.values.get({
     auth: auth,
     spreadsheetId: SPREADSHEET_ID,
-    range: "sheet11!B1:B98",
+    range: "sheet12!B1:B186",
   }, function(err, response) {
     if (err) {
       console.log('The API returned an error: ' + err);
